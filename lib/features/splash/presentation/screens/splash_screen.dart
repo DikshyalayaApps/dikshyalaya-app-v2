d09d2d5fafa8f76a609e8c889670338eb64a50c9
npalien4u <npalien4u@gmail.com>
2024-11-14 11:54:46 +0530

    homescreen placeholder created

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,35 +1,65 @@
 // import 'package:dikshyalaya_v2/app/app_localizations.dart';
+import 'package:dikshyalaya_v2/features/home/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dikshyalaya_v2/core/constants/colors.dart';
 import 'package:dikshyalaya_v2/features/splash/presentation/widgets/splash_animation.dart';
 
-class SplashScreen extends StatelessWidget {
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    //AppLocalizations.of(context)?.loadScreenLocalization('splash_screen');
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
 
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize animation
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..forward();
+
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+
+    // Start timer to navigate to HomeScreen after 3 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      // Check if the widget is still mounted before navigating
+      if (mounted) {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.darkBlue,
+      backgroundColor: Colors.blue.shade900,
       body: Center(
-        child: SplashAnimation(
+        child: FadeTransition(
+          opacity: _animation,
           child: Container(
-            padding: const EdgeInsets.all(8.0), // Space between border and logo
-            decoration: BoxDecoration(
+            width: 150,
+            height: 150,
+            decoration: const BoxDecoration(
               shape: BoxShape.circle,
-              border: Border.all(
-                  color: Colors.white, width: 1.0), // White circular border
-            ),
-            child: Container(
-              width: 150, // Adjust based on logo size
-              height: 150,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage('assets/images/logo.png'),
-                  fit: BoxFit.contain, // Ensures the entire image is visible within the border
-                ),
+              image: DecorationImage(
+                image: AssetImage('assets/images/logo.png'),
+                fit: BoxFit.contain,
               ),
             ),
           ),
@@ -38,3 +68,38 @@ class SplashScreen extends StatelessWidget {
     );
   }
 }
+// class SplashScreen extends StatelessWidget {
+//   const SplashScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     //AppLocalizations.of(context)?.loadScreenLocalization('splash_screen');
+
+//     return Scaffold(
+//       backgroundColor: AppColors.darkBlue,
+//       body: Center(
+//         child: SplashAnimation(
+//           child: Container(
+//             padding: const EdgeInsets.all(8.0), // Space between border and logo
+//             decoration: BoxDecoration(
+//               shape: BoxShape.circle,
+//               border: Border.all(
+//                   color: Colors.white, width: 1.0), // White circular border
+//             ),
+//             child: Container(
+//               width: 150, // Adjust based on logo size
+//               height: 150,
+//               decoration: const BoxDecoration(
+//                 shape: BoxShape.circle,
+//                 image: DecorationImage(
+//                   image: AssetImage('assets/images/logo.png'),
+//                   fit: BoxFit.contain, // Ensures the entire image is visible within the border
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
