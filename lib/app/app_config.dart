14ae0102dcc0c46276446a90fd43637e97bf9ceb
Alien Shrestha <npalien4u@gmail.com>
2024-12-09 17:38:15 +0530

    source update before build

--- lib/app/app_config.dart
+++ lib/app/app_config.dart
@@ -1,22 +1,25 @@
 // core/config/config.dart
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
-  static String get baseUrl {
-    final url = dotenv.env['BASE_URL'];
-    if (url == null || url.isEmpty) {
-      throw Exception("BASE_URL is not defined in the .env file");
-    }
-    return url;
-  }
+  static String baseUrl = "https://api1.dikshyalaya.com/";
+  static String appId = "asdf-asdf-asdf";
 
-  static String get appId {
-    final id = dotenv.env['APP_ID'];
-    if (id == null || id.isEmpty) {
-      throw Exception("APP_ID is not defined in the .env file");
-    }
-    return id;
-  } 
+  // static String get baseUrl {
+  //   final url = dotenv.env['BASE_URL'];
+  //   if (url == null || url.isEmpty) {
+  //     throw Exception("BASE_URL is not defined in the .env file");
+  //   }
+  //   return url;
+  // }
+
+  // static String get appId {
+  //   final id = dotenv.env['APP_ID'];
+  //   if (id == null || id.isEmpty) {
+  //     throw Exception("APP_ID is not defined in the .env file");
+  //   }
+  //   return id;
+  // }
 
   // Add other environment-specific getters if needed
 }
